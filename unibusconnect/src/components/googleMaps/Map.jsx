f95fac9be63367248cb15879272f611dd1e1641d
charbel-98 <charbel_cg@outlook.com>
2023-12-07 20:27:42 +0200

    places input

--- unibusconnect/src/components/googleMaps/Map.jsx
+++ unibusconnect/src/components/googleMaps/Map.jsx
@@ -1,8 +1,7 @@
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 import { useRef, useMemo, useState } from "react";
 const Map = () => {
-  const [lat, setLat] = useState(null);
-  const [lng, setLng] = useState(null);
+  const [home, setHome] = useState(null);
 
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: "AIzaSyDlXmTnz1ntfdhkloeT7HZ2jtJ0fWQPgos",
@@ -16,8 +15,12 @@ const Map = () => {
   }
 
   function showPosition(position) {
-    setLat(position.coords.latitude);
-    setLng(position.coords.longitude);
+    setHome((prev) => {
+      return { ...prev, lat: position.coords.latitude };
+    });
+    setHome((prev) => {
+      return { ...prev, lng: position.coords.longitude };
+    });
     console.log(
       "Latitude: " +
         position.coords.latitude +
@@ -45,24 +48,38 @@ const Map = () => {
   const mapRef = useRef();
   const center = useMemo(
     () => ({
-      lat: lat ? parseFloat(lat) : 34.3963159,
-      lng: lng ? parseFloat(lng) : 35.89584450000007,
+      lat: home?.lat ? parseFloat(home?.lat) : 34.3963159,
+      lng: home?.lng ? parseFloat(home.lng) : 35.89584450000007,
     }),
     // () => ({ lat: 34.3963159, lng: 35.89584450000007 }),
-    [lat, lng]
+    [home?.lat, home?.lng]
   );
   getLocation();
   if (!isLoaded) return <div>loading...</div>;
   return (
-    <GoogleMap
-      zoom={15}
-      center={lat && lng && center}
-      options={{ disableDefaultUI: true }}
-      mapContainerClassName="map-container"
-    >
-      <button onClick={getLocation}></button>
-      {lat && lng && <Marker position={center}></Marker>}
-    </GoogleMap>
+    <>
+      <div className="controls">
+        <h1>Commute?</h1>
+        <Places
+          setHome={(position) => {
+            setHome(position);
+            mapRef.current?.panTo(position);
+          }}
+        />
+        {!home && <p>Enter the address of your office.</p>}
+        {/* {directions && <Distance leg={directions.routes[0].legs[0]} />} */}
+      </div>
+      <GoogleMap
+        zoom={15}
+        center={{ lat: 34.396663, lng: 35.8426649 }}
+        options={{ disableDefaultUI: true }}
+        mapContainerClassName="map-container"
+      >
+        <button onClick={getLocation}>get current location</button>
+        {home?.lat && home?.lng && <Marker position={center}></Marker>}
+        <Marker position={{ lat: 34.396663, lng: 35.8426649 }}></Marker>
+      </GoogleMap>
+    </>
   );
 };
 
