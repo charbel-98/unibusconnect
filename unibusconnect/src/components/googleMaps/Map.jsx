42826f831333880432b759d5f85a7a0e576a2e24
charbel-98 <charbel_cg@outlook.com>
2023-12-07 23:11:34 +0200

    finished the map component

--- unibusconnect/src/components/googleMaps/Map.jsx
+++ unibusconnect/src/components/googleMaps/Map.jsx
@@ -1,10 +1,18 @@
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
-import { useRef, useMemo, useState } from "react";
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  DirectionsRenderer,
+} from "@react-google-maps/api";
+import { useRef, useMemo, useState, useCallback, useEffect } from "react";
+import Places from "./Places";
+const MAPS_LIBRARIES = ["places"];
 const Map = () => {
   const [home, setHome] = useState(null);
-
+  const [directions, setDirections] = useState();
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: "AIzaSyDlXmTnz1ntfdhkloeT7HZ2jtJ0fWQPgos",
+    libraries: MAPS_LIBRARIES,
   });
   function getLocation() {
     if (navigator.geolocation) {
@@ -54,27 +62,63 @@ const Map = () => {
     // () => ({ lat: 34.3963159, lng: 35.89584450000007 }),
     [home?.lat, home?.lng]
   );
-  getLocation();
+  const onLoad = useCallback((map) => (mapRef.current = map), []);
+  const fetchDirections = (uni) => {
+    if (!home) return;
+
+    const service = new google.maps.DirectionsService();
+    service.route(
+      {
+        origin: uni,
+        destination: home,
+        travelMode: google.maps.TravelMode.DRIVING,
+      },
+      (result, status) => {
+        if (status === "OK" && result) {
+          setDirections(result);
+          console.log(result);
+        }
+      }
+    );
+  };
+  useEffect(() => {
+    mapRef.current?.panTo(home);
+    console.log("after");
+    fetchDirections({ lat: 34.396663, lng: 35.8426649 });
+    console.log("finally");
+  }, [home]);
   if (!isLoaded) return <div>loading...</div>;
   return (
     <>
-      <div className="controls">
-        <h1>Commute?</h1>
-        <Places
-          setHome={(position) => {
-            setHome(position);
-            mapRef.current?.panTo(position);
-          }}
-        />
-        {!home && <p>Enter the address of your office.</p>}
-        {/* {directions && <Distance leg={directions.routes[0].legs[0]} />} */}
-      </div>
       <GoogleMap
         zoom={15}
-        center={{ lat: 34.396663, lng: 35.8426649 }}
+        center={center}
         options={{ disableDefaultUI: true }}
         mapContainerClassName="map-container"
+        onLoad={onLoad}
       >
+        <div className="controls">
+          <Places
+            setHome={(position) => {
+              setHome(position);
+              console.log("here");
+            }}
+          />
+
+          {/* {directions && <Distance leg={directions.routes[0].legs[0]} />} */}
+        </div>
+        {directions && (
+          <DirectionsRenderer
+            directions={directions}
+            options={{
+              polylineOptions: {
+                zIndex: 50,
+                strokeColor: "#d9534f",
+                strokeWeight: 5,
+              },
+            }}
+          />
+        )}
         <button onClick={getLocation}>get current location</button>
         {home?.lat && home?.lng && <Marker position={center}></Marker>}
         <Marker position={{ lat: 34.396663, lng: 35.8426649 }}></Marker>
--- unibusconnect/src/components/googleMaps/Places.jsx
+++ unibusconnect/src/components/googleMaps/Places.jsx
@@ -10,6 +10,7 @@ import {
   ComboboxOption,
 } from "@reach/combobox";
 import "@reach/combobox/styles.css";
+import { useEffect } from "react";
 
 export default function Places({ setHome }) {
   const {
@@ -28,7 +29,10 @@ export default function Places({ setHome }) {
     const { lat, lng } = await getLatLng(results[0]);
     setHome({ lat, lng });
   };
-
+  //   const filter = JSON.parse(localStorage.filter);
+  //   useEffect(() => {
+  //     setValue("arde");
+  //   }, []);
   return (
     <Combobox onSelect={handleSelect}>
       <ComboboxInput
@@ -36,7 +40,7 @@ export default function Places({ setHome }) {
         onChange={(e) => setValue(e.target.value)}
         disabled={!ready}
         className="combobox-input"
-        placeholder="Search office address"
+        placeholder="Search your home address"
       />
       <ComboboxPopover>
         <ComboboxList>
